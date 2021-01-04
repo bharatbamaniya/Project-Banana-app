55211ba5b06508e9d2ba9d289efbcd9c954ae8d6
Bharat Kumar <bkumarm048@gmail.com>
2021-01-04 17:50:43 +0530

    display output and error handling

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTraslationURL(text) {
     return serverURL + "?" + "text=" + text;
 }
 
+function errorHandler(error) {
+    console.log("error occured",error)
+    alert("somethign wrong with server! try again after some time...")
+}
 
 function clickEventHandler(){
 
     var inputText = txtInput.value;
 
     fetch(getTraslationURL(inputText))
-    .then(response => response.json())
-    .then(json => console.log(json.contents.translated));
+        .then(response => response.json())
+        .then(json => outputDiv.innerHTML = json.contents.translated)
+        .catch(errorHandler);
+
+    
 }
 
 btnTranslate.addEventListener('click', clickEventHandler );
