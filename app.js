19b060461195b5c271dab6c325dff209abbdacbe
Bharat Kumar <bkumarm048@gmail.com>
2021-01-04 16:52:47 +0530

    input output

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
+
 btnTranslate.addEventListener('click', clickEventHandler );
 
 function clickEventHandler(){
-    console.log('clicked');
-    console.log("input",txtInput.value);
+     outputDiv.innerHTML = 'ajsgjdkdgjk' + txtInput.value;
 }
