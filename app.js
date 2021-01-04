7f9585e8e7161291fcebaa2d98a69996c681758c
Bharat Kumar <bkumarm048@gmail.com>
2021-01-04 17:31:39 +0530

    implementing  translated text from server

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-btnTranslate.addEventListener('click', clickEventHandler );
+function getTraslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
 
 function clickEventHandler(){
-     outputDiv.innerHTML = 'ajsgjdkdgjk' + txtInput.value;
+
+    var inputText = txtInput.value;
+
+    fetch(getTraslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated));
 }
+
+btnTranslate.addEventListener('click', clickEventHandler );
